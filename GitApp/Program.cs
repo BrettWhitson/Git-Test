f4f98ab5a6ddc43cca3cf9886e8927fd575e32d1
bremwhit <bremwhit@CSG1-522513>
2017-09-05 13:53:32 -0500

    random number game

--- GitApp/Program.cs
+++ GitApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Brett Whitson
+// 9/5/17
+// a simple number guessing game
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +14,34 @@ namespace GitApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int randomNum = rnd.Next(1, 10);
+            int guessCount = 1;
+            
+
+            Console.WriteLine("I'm thinking of a number between 1 and 10. Can you guess it?");
             Console.WriteLine();
+
+            int userGuess = Convert.ToInt32(Console.ReadLine());
+
+            while(userGuess != randomNum)
+            {
+                if (userGuess < randomNum)
+                {
+                    Console.WriteLine("Your guess is too low. Try again.");
+                    guessCount++;
+                }
+                else if (userGuess > randomNum)
+                {
+                    Console.WriteLine("Your guess is too high. Try again.");
+                    guessCount++;
+                }
+                
+                userGuess = Convert.ToInt32(Console.ReadLine());
+            }
+            
+                Console.WriteLine("That's correct! It only took " + guessCount + " times!");
+    
         }
     }
 }
